3e724918173ff592f864ea097ae501138384a2c5
Shah-Sultanul-Arefin <sarefin@academic.rrc.ca>
2024-06-12 16:12:30 -0500

    Rebuild project for production deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
+// tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', // or 'class' or you can remove it entirely if not needed
   theme: {
     extend: {
       fontFamily: {
